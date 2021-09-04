35b8dacbf68b3e793818107290b205830d7f0603
zhangyunpeng <zhangyunpeng@sensorsdata.cn>
2021-09-04 14:42:31 +0800

    del: file

--- packages/ts-video-tool/src/index.js
+++ packages/ts-video-tool/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.min.css";
-import "./index.less";
 import M from "./merge.jsx";
 
 ReactDOM.render(<M />, document.getElementById("root"));
