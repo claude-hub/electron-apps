419f6a56ac027e2fc704efd6cf417f6d77834bc8
zhangyunpeng <zhangyunpeng@sensorsdata.cn>
2022-01-12 14:39:11 +0800

    init

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# Electron 小工具集
-
-```
-── packages
-    └── ts-video-tool   ---- TS视频合并转换工具
-```
+# lerna demo
--- packages/ts-video-tool/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "antd/dist/antd.min.css";
-import M from "./merge.jsx";
-
-ReactDOM.render(<M />, document.getElementById("root"));
